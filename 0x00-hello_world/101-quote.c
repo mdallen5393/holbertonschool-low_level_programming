532bcb251191c58f5367c87d0238c47cee680d30
mdallen5393 <allen_matthew@outlook.com>
2022-05-20 12:51:57 -0700

    AT7 test

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,8 +7,8 @@
  */
 int main(void)
 {
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	fwrite(str, sizeof(str), 1, stderr);
+	fwrite(str, sizeof(str) - 1, 1, stderr);
 	return (1);
 }
