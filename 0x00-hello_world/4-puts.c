044418f284c1b25c02025d7dd3878b97ab966132
mdallen5393 <allen_matthew@outlook.com>
2022-05-20 11:44:27 -0700

    Task 4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ *
+ * 4-puts.c - prints line of text using puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
