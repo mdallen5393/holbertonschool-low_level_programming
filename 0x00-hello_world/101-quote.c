92faaa77ba2e10e46a0dcda084afd46dffe68043
mdallen5393 <allen_matthew@outlook.com>
2022-05-20 12:40:09 -0700

    Optional Task 8. UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print string to standard error
+ *
+ * Return: 1
+ */
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(str, sizeof(str), 1, stderr);
+	return (1);
+}
