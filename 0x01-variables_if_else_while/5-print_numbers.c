be520aaaccbde07d254932703db64bd15b70014b
mdallen5393 <allen_matthew@outlook.com>
2022-05-23 10:27:40 -0700

    Task 5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all single digit nums, base 10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
