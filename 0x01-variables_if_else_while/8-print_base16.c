2ec7cd602339fe37de2a847b2f9c662fd974ead1
mdallen5393 <allen_matthew@outlook.com>
2022-05-23 10:40:16 -0700

    Task 8. Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print all numbers of base 16 in lowercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		putchar(i);
+
+	for (i = 'a'; i <= 'f'; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
