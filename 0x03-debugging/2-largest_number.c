5201752352f977e0c4d515089b9370e42160cbaf
mdallen5393 <allen_matthew@outlook.com>
2022-05-29 13:58:16 -0700

    Task 2. 0 > 972?

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,9 +11,9 @@
 int largest_number(int a, int b, int c)
 {
 
-	if (a > b && a > c)
+	if (a >= b && a >= c)
 		return (a);
-	if (b > a && b > c)
+	if (b >= a && b >= c)
 		return (b);
 	return (c);
 }
