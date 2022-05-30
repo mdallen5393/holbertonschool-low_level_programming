a94fe5c19aeb11cb7ec48fcee71906ab324bb4ec
mdallen5393 <allen_matthew@outlook.com>
2022-05-29 21:12:21 -0700

    Task 9. Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers 1 to 100, with "Fizz", "Buzz", and "FizzBuzz"
+ * replacing multiples of 3, 5, and both, respectively.
+ * Return: 0 always.
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+
+		if (i != 100)
+			printf(" ");
+	}
+	printf("\n");
+
+	return (0);
+}
