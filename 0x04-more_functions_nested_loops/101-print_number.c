9b92dc2e8b7869dbf23370b09ba7874836a469f0
mdallen5393 <allen_matthew@outlook.com>
2022-05-31 10:03:09 -0700

    Task 12 correction

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -17,27 +17,20 @@ void print_number(int n)
 	if (curr < 0)
 		curr = -curr;
 
-	if (n == 0)
+	if (n / 10 != 0)
 	{
-		_putchar('0');
-	}
-	else
-	{
-		if (n / 10 != 0)
+		if (n < 0)
 		{
-			if (n < 0)
-			{
-				print_number(n / -10);
-			}
-			else
-			{
-				print_number(n / 10);
-			}
-			_putchar(curr + '0');
+			print_number(n / -10);
 		}
 		else
 		{
-			_putchar(curr + '0');
+			print_number(n / 10);
 		}
+		_putchar(curr + '0');
+	}
+	else
+	{
+		_putchar(curr + '0');
 	}
 }
