3f3d4b6789776e093c66cec2a28a90c42a6b382a
mdallen5393 <allen_matthew@outlook.com>
2022-06-01 12:02:03 -0700

    Task 6. Half the lies they tell about me arent true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,12 +6,14 @@
  */
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
+	int len = 0;
 
-	while (str[i] != '\0')
-	{
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i = i + 2)
 		_putchar(str[i]);
-		i += 2;
-	}
+
 	_putchar('\n');
 }
