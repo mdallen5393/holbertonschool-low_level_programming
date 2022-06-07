bd05aa45bb3a938e7539fbb0aa5d2f49c851d7d3
mdallen5393 <allen_matthew@outlook.com>
2022-06-07 16:00:26 -0700

    Task 7. The line of life is a ragged diagonal between duty and desire

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include "stddef.h"
+#include "stdio.h"
+
+/**
+ * print_diagsums - print sum of the two diagonals of a nxn matrix of ints
+ * @a: input 2D array
+ * @size: dimensions of matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i <= size; i++)
+	{
+		if (i < size)
+			sum1 += *(a + i * (size + 1));
+		sum2 += *(a + i * (size - 1));
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
