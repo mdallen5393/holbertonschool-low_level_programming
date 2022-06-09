005750aa94538beb3ab2342fe4f9c70830a5e9f0
mdallen5393 <allen_matthew@outlook.com>
2022-06-08 18:15:28 -0700

    Task 5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,8 +13,11 @@ int sqrt_rec(int i, int n)
 	if (n < 0)
 		return (-1);
 
-	if (i * i <= n && (i + 1) * (i + 1) > n)
+	if (i * i == n)
 		return (i);
+
+	if (i * i < n && (i + 1) * (i + 1) > n)
+		return (-1);
 
 	next = (i + n / i) / 2;
 	return (sqrt_rec(next, n));
