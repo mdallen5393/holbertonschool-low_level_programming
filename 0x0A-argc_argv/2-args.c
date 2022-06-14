7bf1f206cbdcfade3c8eefc2b7f33768ff63f9cb
mdallen5393 <allen_matthew@outlook.com>
2022-06-14 07:29:23 -0700

    Task 2 correction - Betty

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - prints all arguments recieved
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0 always.
+ */
 int main(int argc, char **argv)
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 		printf("%s\n", argv[i]);
 
