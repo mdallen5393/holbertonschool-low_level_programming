135734592c1cb61ac7a3b7627f640e1b8d19d86a
mdallen5393 <allen_matthew@outlook.com>
2022-06-14 07:32:32 -0700

    Task 0 correction - Betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
+/**
+ * main - prints its name
+ * @argc: number of arguments
+ * @argv: vector of arguments
+ * Return: 0 always.
+ */
 int main(int argc, char **argv)
 {
 	(void)argc;
