d74c9fb4c6da4f40dcd023d6f32a36de3c1e2cc2
mdallen5393 <allen_matthew@outlook.com>
2022-06-16 11:37:29 -0700

    Task 0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+#include <stdio.h>
 
 /**
  * create_array - creates an array of chars, and initializes w/spec. char
@@ -13,7 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (arr == NULL)
+	{
+		fprintf("failed to allocate memory");
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 	{
