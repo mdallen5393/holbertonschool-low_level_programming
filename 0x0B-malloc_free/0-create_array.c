f3e7cba4bee076aeae452b0feefb3b7266a2e182
mdallen5393 <allen_matthew@outlook.com>
2022-06-16 11:45:57 -0700

    Task 0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,15 +14,10 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (arr == NULL || size == 0)
-	{
-		printf("failed to allocate memory\n");
 		return (NULL);
-	}
 
 	for (i = 0; i < size; i++)
-	{
 		arr[i] = c;
-	}
 
 	return (arr);
 }
