ac6c218220101d031624b058b92c19dcb272cc13
mdallen5393 <allen_matthew@outlook.com>
2022-06-21 10:11:36 -0700

    task 2 correction

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -18,10 +18,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
-	{
-		free(ptr);
 		return (NULL);
-	}
 
 	for (i = 0; i < nmemb; i++)
 		ptr[i] = 0;
