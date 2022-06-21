022d6775dd2b01e304400aae53d673f84ca3960d
mdallen5393 <allen_matthew@outlook.com>
2022-06-21 11:42:42 -0700

    Task 1 correction

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -26,7 +26,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		while (s2[l2])
 			l2++;
 
-		if (l2 >= n)
+		if (l2 > n)
 			l2 = n;
 	}
 
@@ -42,6 +42,9 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		str[i] = s2[j];
 
 	str[i] = '\0';
+
+	if (s1 == NULL && s2 == NULL)
+		return (NULL);
 
 	return (str);
 }
