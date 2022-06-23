4656306855beeb4536e67a96c32516cca6a07730
mdallen5393 <allen_matthew@outlook.com>
2022-06-23 12:23:15 -0700

    Task 0 correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - function that prints a name, choosing which print to use
  * @name: pointer to name to print
- * @f: function pointer to function that takes a character pointer and returns nothing
+ * @f: function pointer to function that takes a character pointer
  */
 void print_name(char *name, void (*f)(char *))
 {
