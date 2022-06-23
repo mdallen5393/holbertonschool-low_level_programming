dd365c03643513ec3ea15066cc872266a0fe2b28
mdallen5393 <allen_matthew@outlook.com>
2022-06-23 12:31:45 -0700

    Task 1 correction

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -13,7 +13,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array == NULL || action == NULL)
 		return;
 
-	if (size == 0 || size == NULL)
+	if (size == 0)
 		return;
 
 	for (i = 0; i < size; i++)
