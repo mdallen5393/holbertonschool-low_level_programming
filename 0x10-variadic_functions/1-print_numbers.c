ce7be111a493a0f4f93b7880b1b52acb1dcc08cb
mdallen5393 <allen_matthew@outlook.com>
2022-06-28 10:07:57 -0700

    Task 1 correction

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -9,7 +9,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-
+	if (n == 0)
+	{
+		return;
+	}
 	va_start(ap, n);
 
 	for (i = 0; i < n - 1; i++)
