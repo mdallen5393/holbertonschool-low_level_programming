eb2ad4f8a4184543a611244bcccc7f7bbc2a1a06
mdallen5393 <allen_matthew@outlook.com>
2022-07-07 11:47:46 -0700

    Task 4. Free list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list.
+ * @head: list to free
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp->next);
+		free(temp);
+	}
+
+	free(head);
+}
