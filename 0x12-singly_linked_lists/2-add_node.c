1bc1e3fd2f780398b2778c23e6cb1b4dcdd1c0ec
mdallen5393 <allen_matthew@outlook.com>
2022-07-07 11:49:46 -0700

    Task 2 correction to add len to new node

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *newNode;
 
 	newNode = malloc(sizeof(list_t));
-	
+
 	if (!newNode)
 	{
 		free(newNode);
@@ -21,6 +21,7 @@ list_t *add_node(list_t **head, const char *str)
 	newNode->next = *head;
 	*head = newNode;
 	(*head)->str = strdup(str);
+	(*head)->len = strlen(str);
 	(*head)->next = newNode->next;
 
 	return (*head);
