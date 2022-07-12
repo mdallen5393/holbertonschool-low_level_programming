0a5e892a329379308a3c09ab03412c8053f8d093
mdallen5393 <allen_matthew@outlook.com>
2022-07-12 08:18:44 -0700

    Task 9 correction

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,6 +18,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newNode->n = n;
 
+	if (idx == 0)
+	{
+		newNode->next = *head;
+		*head = newNode;
+		return (newNode);
+	}
+
 	while (temp)
 	{
 		if (i == idx - 1)
