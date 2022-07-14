159bb21cfb38bfb8db79d5dc16832b1752d95189
mdallen5393 <allen_matthew@outlook.com>
2022-07-13 17:34:16 -0700

    Task 2. 10

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,8 +8,24 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
+	unsigned int i = 0;
+
+	if (n <= 1)
+		return (n % 2);
+
+	while (n >= 1)
+	{
+		if (i >= index)
+			break;
+		n /= 2;
+		i++;
+	}
+
+	if (index > i && index < 32)
+		return (0);
+
 	if (index > 31)
 		return (-1);
 
-	return ((n >> index) & 1);
+	return (n % 2);
 }
