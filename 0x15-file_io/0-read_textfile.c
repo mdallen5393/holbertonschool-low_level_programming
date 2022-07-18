62446a26fd138206b9ac47e031800ebc59366da6
mdallen5393 <allen_matthew@outlook.com>
2022-07-18 11:35:44 -0700

    Task 0 correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,7 +19,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (!buffer)
 		return (0);
 
-	fd = open(filename, O_CREAT | O_RDONLY);
+	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
