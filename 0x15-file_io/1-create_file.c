10cdd7f55fca150cb8ce52a3f5b7f6e644b664ad
mdallen5393 <allen_matthew@outlook.com>
2022-07-18 12:44:57 -0700

    Task 1 correction

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
-	if (!text_content)
+	if (text_content)
 	{
 		if (write(fd, text_content, strlen(text_content)) == -1)
 			return (-1);
