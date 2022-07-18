00d018f555a23d2b91ac0c6bac07480055aacc68
mdallen5393 <allen_matthew@outlook.com>
2022-07-18 11:53:43 -0700

    Task 0 correction

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,25 +17,20 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	buffer = malloc(sizeof(*buffer) * (letters));
 	if (!buffer)
-	{
-		free(buffer);
 		return (0);
-	}
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
 
 	bytes = read(fd, buffer, letters);
+	close (fd);
+
 	if (bytes == -1)
 	{
 		free(buffer);
-		close(fd);
 		return (0);
 	}
-
-	close(fd);
-	/*buffer[letters] = '\0';*/
 
 	if (write(1, buffer, bytes) != bytes)
 	{
