b898fc4623f63eb7a956e65f0ae05936f2b97645
mdallen5393 <allen_matthew@outlook.com>
2022-08-02 16:19:49 -0700

    task 5 correction

--- 0x18-doubly_linked_lists/5-get_dnodeint.c
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -8,14 +8,15 @@
  */
  dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
  {
-    unsigned int i = 0;
-    list *temp = head;
+	unsigned int i = 0;
+	list *temp = head;
 
-    while (temp && i < index)
-    {
-        temp = temp->next;
-        i++;
-    }
-    
-    return (temp);
+	while (temp && i < index)
+	{
+		temp = temp->next;
+		i++;
+	}
+
+	return (temp);
  }
+ 
