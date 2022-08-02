35b7366fe44f3769967a794252b0c05c8ff55659
mdallen5393 <allen_matthew@outlook.com>
2022-08-02 14:45:47 -0700

    betty corrections to lists.h file

--- 0x18-doubly_linked_lists/lists.h
+++ 0x18-doubly_linked_lists/lists.h
@@ -21,9 +21,9 @@
  */
 typedef struct dlistint_s
 {
-    int n;
-    struct dlistint_s *prev;
-    struct dlistint_s *next;
+	int n;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 
 /* --- FUNCTION PROTOTYPES --- */
