7d9e271ef8014c14c237357be6650d340f437891
mdallen5393 <allen_matthew@outlook.com>
2022-08-02 15:54:37 -0700

    task 2 correction

--- 0x18-doubly_linked_lists/2-add_dnodeint.c
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -13,6 +13,8 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (!newNode)
 		return (NULL);
 
+	if (!head)
+		return (NULL);
 	newNode->n = n;
 	newNode->next = *head;
 	newNode->prev = NULL;
