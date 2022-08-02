feca0bee5abd4ae9af005e1a319e5ffb0c94f084
mdallen5393 <allen_matthew@outlook.com>
2022-08-02 14:44:01 -0700

    Task 0 correction

--- 0x18-doubly_linked_lists/0-print_dlistint.c
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -7,17 +7,17 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t numNodes = 0;
+	size_t numNodes = 0;
 
-    if (!h)
-        return (0);
-        
-    while (h)
-    {
-        printf("%i\n", h->n);
-        numNodes++;
-        h = h->next;
-    }
+	if (!h)
+		return (0);
 
-    return (numNodes);
+	while (h)
+	{
+		printf("%i\n", h->n);
+		numNodes++;
+		h = h->next;
+	}
+
+	return (numNodes);
 }
