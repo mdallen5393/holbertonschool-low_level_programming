4c06628e90d8f5270f581df0fa0e3db3d4a4ccf1
mdallen5393 <allen_matthew@outlook.com>
2022-08-02 17:30:20 -0700

    Task 8 correction

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -23,8 +23,8 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 			*head = temp->next;
 			(*head)->prev = NULL;
 		}
-		else
-			*head = NULL;
+		/* else */
+			/* *head = NULL; */
 
 		free(temp);
 		return (1);
