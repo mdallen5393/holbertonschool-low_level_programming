904e64dbd57264cd38674db65f05ecd405b82572
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 15:12:55 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -9,5 +9,41 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	unsigned long int index;
+	hash_node_t *newNode;
+	hash_node_t *curr;
 
+	if (!ht || !key || !value)
+		return (0);
+
+	if (strcmp(key, "\0") == 0)
+		return (0);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	curr = ht->array[index];
+	
+	newNode = malloc(sizeof(hash_node_t));
+	if (!newNode)
+		return (0);
+
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+	newNode->next = NULL;
+
+	if (curr)
+	{
+		while (curr->next)
+		{
+			if(strcmp(curr->key, key) == 0)
+			{
+				free(newNode);
+				return (1);
+			}
+			curr = curr->next;
+		}
+		curr->next = newNode;
+		return (1);
+	}
+	curr = newNode;
+	return (1);
 }
