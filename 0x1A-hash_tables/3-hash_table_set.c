6a4831a500adb5da8a846e2b6d69a2f3007ee3aa
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 15:47:02 -0700

    Task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -56,6 +56,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (1);
 	}
 	/* Set the array[index] to the newNode */
-	curr = newNode;
+	ht->array[index] = newNode;
 	return (1);
 }
