44d5b493e0273c2f6416b3b20aeb9f61163e12fa
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 20:25:39 -0700

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -8,5 +8,18 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int i;
+	hash_node_t *curr;
 
+	for (i = 0; i < ht->size; i++)
+	{
+		curr = ht->array[i];
+		while (curr)
+		{
+			if (strcmp(curr->key, key) == 0)
+				return (curr->value);
+			curr = curr->next;
+		}
+	}
+	return (NULL);
 }
