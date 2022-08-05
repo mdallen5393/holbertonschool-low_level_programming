60493492c3304b4cdaa8dabe32696c3dff8a24c8
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 21:42:32 -0700

    Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,5 +6,36 @@
  */
 void hash_table_print(const hash_table_t *ht)
 {
+	unsigned long int i, count = 0;
+	hash_node_t *curr;
 
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+				count++;
+				curr = curr->next;
+			}
+		}
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			curr = ht->array[i];
+			while (curr)
+			{
+				printf("'%s': '%s'", curr->key, curr->value);
+				if (i < count)
+					printf(", ");
+				curr = curr->next;
+			}
+		}
+	}
+	printf("}\n");
 }
