2996631e93778e851a71a0b21c70e9bc900a722b
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 21:50:48 -0700

    Task 6

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,5 +6,23 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-
+	unsigned long int i;
+	hash_node_t *curr;
+	
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array[i])
+		{
+			while (curr)
+			{
+				curr = ht->array[i]->next;
+				free(ht->array[i]->key);
+				free(ht->array[i]->value);
+				free(ht->array[i]);
+				ht->array[i] = curr;
+			}
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
