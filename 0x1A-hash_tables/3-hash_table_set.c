b457381c01a5191babbe0c663561b0a147ae4e4e
mdallen5393 <allen_matthew@outlook.com>
2022-08-04 20:43:18 -0700

    Task 3 correction

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -16,9 +16,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!ht || !key)
 		return (0);
 
-	if (strcmp(key, "\0") == 0)
-		return (0);
-
 	index = key_index((const unsigned char *)key, ht->size);
 	curr = ht->array[index];
 
