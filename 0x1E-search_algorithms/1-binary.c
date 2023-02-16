8d00248f75a7a05916e526c55cd8cf42a2106b1b
mdallen5393 <allen_matthew@outlook.com>
2023-02-15 21:42:40 -0600

    Initial commit for project 0x0E. Searching Algorithms 0

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - function that searches for a value in an array of
+ * integers using the Linear search algorithm
+ *
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in `array`
+ * @value: the value to search for
+ * Return: -1 if `value` is not present or if `array` is NULL; else returns
+ * the index where the value was found.
+ */
+int binary_search(int *array, size_t size, int value)
+{
+
+}
