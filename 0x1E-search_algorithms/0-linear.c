e92f7f1e0a92f55eb9002de5979d8eb6436f8ea9
mdallen5393 <allen_matthew@outlook.com>
2023-02-15 21:51:33 -0600

    Task 0. Linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,16 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    int idx;
+	int i;
 
-    return (idx);
+	if (!array)
+		return (-1);
+
+	for (i = 0; i < (int)size; i++)
+	{
+		printf("Value checked array[%d] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
